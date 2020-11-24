7f0129dc32d9cebb7e38679e6d23bb300b9c8c4d
Kaspar Metsa <kaspar.metsa@helmes.com>
2020-11-24 08:03:43 +0100

    First commit

---
+++ src/components/opt-form/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Button, Container, Input, Text, Break} from './styles/opt-form'
+
+export default function OptForm({children}) {
+    return <Container>{children}</Container>
+}
+
+OptForm.Input = function OptFormInput({children, ...restProps}) {
+    return <Input {...restProps}/>;
+}
+
+OptForm.Button = function OptFormButton({children}) {
+    return (
+        <Button>
+            {children} <img src="/images/icons/chevron-right.png" alt="Try now"/>
+        </Button>
+    )
+}
+
+OptForm.Text = function OptFormText({children}) {
+    return <Text>{children}</Text>
+}
+OptForm.Break = function OptFormBreak() {
+    return <Break/>;
+}
